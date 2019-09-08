c04a061ccb01f2859abd8916ec722742b589a888
soozical <susie.odneal@gmail.com>
2019-09-08 11:27:44 -0500

    timer working, fixed layering issues

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float timeLeft;
+
+    public string timeString;
+    public TextMeshProUGUI timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        int timeRound = (int)timeLeft;
+        timeString = timeRound.ToString();
+        timerText.SetText(timeString);
+    }
+}
